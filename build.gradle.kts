0b12eaa71e3c9e8abd15e97e63be9cae5c6eeacf
Sébastien <sebastien.vial@etu.umontpellier.fr>
2022-08-19 11:10:30 +0200

    build: fix & `maven-publish` for local

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    id("java-library")
+    `java-library`
+    `maven-publish`
 }
+
+group   = "wtf.mizu"
+version = "0.1.0"
 
 repositories {
     // Most of the libraries designed by Mizu are published on maven
@@ -12,4 +16,12 @@ dependencies {
     implementation("com.google.guava", "guava", "31.1-jre")
     implementation("com.squareup", "javapoet", "1.13.0")
     implementation("org.jetbrains", "annotations", "23.0.0")
+}
+
+publishing {
+    publications {
+        create("mavenJava", MavenPublication::class.java) {
+            from(components["java"])
+        }
+    }
 }
