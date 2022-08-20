5f05c559ef18ad67f2105e4a7311dbb8b668d92d
Sébastien <sebastien.vial@etu.umontpellier.fr>
2022-08-20 12:48:46 +0200

    build: guava, javapoet as `api`

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.guava", "guava", "31.1-jre")
-    implementation("com.squareup", "javapoet", "1.13.0")
+    api("com.google.guava", "guava", "31.1-jre")
+    api("com.squareup", "javapoet", "1.13.0")
     implementation("org.jetbrains", "annotations", "23.0.0")
 }
 
